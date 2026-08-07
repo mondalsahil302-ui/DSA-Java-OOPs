799c1ed75a7e401a579dcdb88592c7f950cf60a0
MD Sahil <mondalsahil302@gmail.com>
2026-08-08 00:13:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub - Time: O(1), Space: O(1)

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int a=5;
+        int b=7;
+        int c= a+b;
+        System.out.println(c);
+        return new int[5];
+        
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
