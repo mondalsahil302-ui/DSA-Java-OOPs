74d560dca86b968caaf2a3a34a469ded6d321f79
mondalsahil302-ui <mondalsahil302@gmail.com>
2026-08-08 00:39:04 +0530

    pattern pascal

---
+++ pattern.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class pattern
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of rows: ");
+        int n = sc.nextInt();
+
+        for(int i = 0; i < n; i++)
+        {
+            // Print spaces
+            for(int j = 0; j < n - i - 1; j++)
+            {
+                System.out.print("  ");
+            }
+
+            int num = 1;
+
+            // Print Pascal numbers
+            for(int j = 0; j <= i; j++)
+            {
+                System.out.print(num + "   ");
+
+                num = num * (i - j) / (j + 1);
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
