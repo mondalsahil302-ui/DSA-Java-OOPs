bc68ef3cfc611d842dab4b21c71174e87fe78c31
mondalsahil302-ui <mondalsahil302@gmail.com>
2026-08-08 16:55:43 +0530

    string

---
+++ name print.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class name
+{
+    public static void main(String[]args)
+    {
+        Scanner sc = new Scanner (System.in);
+        System.out.println (" Enter the name :");
+        String name = sc.next();
+         System.out.println (" the name :" + name);
+
+    }
+}
