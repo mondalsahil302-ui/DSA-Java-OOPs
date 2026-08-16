9b5c739d62225a78d09ec82774d2bf60468cc86d
MD Sahil <mondalsahil302@gmail.com>
2026-08-16 18:11:34 +0530

    Time: 0 ms (100%), Space: 46.4 MB (62.99%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 1470-shuffle-the-array/1470-shuffle-the-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] shuffle(int[] nums, int n) {
+
+        int[] ans = new int[2 * n];
+
+        for (int i = 0; i < n; i++) {
+            ans[i*2] = nums[i];
+            ans[2*i+1] = nums[i + n];
+        }
+
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
